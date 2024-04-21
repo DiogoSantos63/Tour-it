801a2d2b5c951cae385863e85693bb47d9671ca7
Diogo Moura Marques <diogodemouramarques2004@gmail.com>
2024-04-21 13:14:07 +0100

    Parte 3 finalização do ui

--- B_features/src/main/java/com/example/features/homepage/ui/HomePageScreen.kt
+++ B_features/src/main/java/com/example/features/homepage/ui/HomePageScreen.kt
@@ -1,24 +1,29 @@
 package com.example.features.homepage.ui
 
+import com.example.features.homepage.ui.compose.GABottomBarNavigation
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -49,35 +54,33 @@ fun HomePageScreen() {
         topBar = {
             Surface(
                 color = Color(0xFF313131),
-
             ) {
-                MediumTopAppBar(
-                    title = {
+                Column {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .padding(start = 100.dp)
+                    ) {
                         Image(
                             painter = painterResource(id = R.drawable.logo_tour_it),
                             contentDescription = null,
                             modifier = Modifier
-                                .padding(start = 100.dp, top = 16.dp)
                                 .size(170.dp)
-
+                                .padding(start = 16.dp)
                         )
-                    },
-                    actions = {
+                        Spacer(modifier = Modifier.width(50.dp))
                         GAProfileCircle(
-                            imageUrl = "https://publish-p47754-e237306.adobeaemcloud.com/adobe/dynamicmedia/deliver/dm-aid--914bcfe0-f610-4610-a77e-6ea53c53f630/_330603286208.app.png?preferwebp=true&width=312",
-                            modifier = Modifier.padding(top = 16.dp)
+                            image = R.drawable.sem_t_tulo,
+                            modifier = Modifier
+                                .padding(16.dp)
                         )
-                    },
-                    modifier = Modifier
-                        .background(Color(0xFF313131))
-
-                )
+                    }
+                }
             }
-
         },
         content = {
             Column(
-                modifier = Modifier.padding(top = 140.dp, start = 16.dp, end = 16.dp)
+                modifier = Modifier.padding(top = 190.dp, start = 16.dp, end = 16.dp)
             ) {
                 Column(modifier = Modifier.padding(end = 100.dp)) {
                     Row(
@@ -181,6 +184,38 @@ fun HomePageScreen() {
                         }
                     }
                 }
+            }
+        },
+        bottomBar = {
+            /*Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
+
+                ) {
+                Box(
+                    modifier = Modifier
+                        .size(70.dp)
+                        .background(Color.LightGray, shape = CircleShape)
+                )
+
+                Box(
+                    modifier = Modifier
+                        .size(60.dp)
+                        .background(Color.Black, shape = CircleShape)
+                        .align(Alignment.Center)
+                )
+
+                Icon(
+                    imageVector = Icons.Default.LocationOn,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(30.dp)
+                        .align(Alignment.Center),
+                    tint = Color.White
+                )
+            }*/
+            Surface(shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)) {
+                GABottomBarNavigation()
             }
         }
     )
--- B_features/src/main/java/com/example/features/homepage/ui/compose/GAProfileCircle.kt
+++ B_features/src/main/java/com/example/features/homepage/ui/compose/GAProfileCircle.kt
@@ -1,5 +1,6 @@
 package com.example.features.homepage.ui.compose
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
@@ -8,25 +9,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import coil.compose.AsyncImage
+import com.example.b_features.R
 
 @Composable
-fun GAProfileCircle(imageUrl: String, modifier: Modifier = Modifier) {
+fun GAProfileCircle(image: Int, modifier: Modifier = Modifier) {
     Box {
-        Box(
-            modifier = Modifier
-                .size(28.dp)
-                .clip(CircleShape)
-                .background(Color.Black)
-                .zIndex(-1f)
-        )
-        AsyncImage(
-            model = imageUrl,
+        Image(
+            painter = painterResource(id = image),
             contentDescription = null,
             modifier = Modifier
-                .size(28.dp)
+                .size(50.dp)
                 .clip(CircleShape)
         )
     }
