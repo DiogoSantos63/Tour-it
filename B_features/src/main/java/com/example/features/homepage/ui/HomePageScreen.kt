fda644d68c1a187696e7ae24761f4c00d70669d4
Diogo Moura Marques <diogodemouramarques2004@gmail.com>
2024-04-22 14:43:56 +0100

    Parte 4 finalização do ui

--- B_features/src/main/java/com/example/features/homepage/ui/HomePageScreen.kt
+++ B_features/src/main/java/com/example/features/homepage/ui/HomePageScreen.kt
@@ -1,25 +1,19 @@
 package com.example.features.homepage.ui
 
-import com.example.features.homepage.ui.compose.GABottomBarNavigation
+import com.example.producer.components.GABottomBarNavigation
 import android.annotation.SuppressLint
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
-import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -32,7 +26,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -41,8 +34,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.b_features.R
-import com.example.features.homepage.ui.compose.GAProfileCircle
 import com.example.features.homepage.ui.compose.GAShowProduct
+import com.example.producer.components.GATopBar
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -52,31 +45,7 @@ fun HomePageScreen() {
     Scaffold(
         containerColor = Color(0xFF313131),
         topBar = {
-            Surface(
-                color = Color(0xFF313131),
-            ) {
-                Column {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier
-                            .padding(start = 100.dp)
-                    ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.logo_tour_it),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(170.dp)
-                                .padding(start = 16.dp)
-                        )
-                        Spacer(modifier = Modifier.width(50.dp))
-                        GAProfileCircle(
-                            image = R.drawable.sem_t_tulo,
-                            modifier = Modifier
-                                .padding(16.dp)
-                        )
-                    }
-                }
-            }
+            GATopBar()
         },
         content = {
             Column(
--- E_producer/src/main/java/com/example/producer/components/GABottomBarNavigation.kt
+++ E_producer/src/main/java/com/example/producer/components/GABottomBarNavigation.kt
@@ -1,16 +1,12 @@
-package com.example.features.homepage.ui.compose
+package com.example.producer.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
@@ -26,9 +22,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Surface
-import androidx.compose.ui.graphics.Color
 
 @Composable
 fun GABottomBarNavigation(
