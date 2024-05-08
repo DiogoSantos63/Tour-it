fc521b6aa5a84e6e4e0c21a9108aa84b7ac51218
Diogo Moura Marques <diogodemouramarques2004@gmail.com>
2024-05-08 21:30:19 +0100

    Screen cart

--- B_features/src/main/java/com/tour_it/features/cart/CartScreen.kt
+++ B_features/src/main/java/com/tour_it/features/cart/CartScreen.kt
@@ -3,8 +3,10 @@ package com.tour_it.features.cart
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
@@ -22,6 +24,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -126,9 +129,16 @@ fun CartScreen() {
                         .padding(end = 270.dp, top = 4.dp)
                 )
                 Spacer(modifier = Modifier.height(28.dp))
-                CartCompose()
-                CartCompose()
-                CartCompose()
+                Box(modifier = Modifier
+                    .clip(RoundedCornerShape(8.dp))
+                ){
+                    Column {
+                        CartCompose()
+                        CartCompose()
+                        CartCompose()
+                    }
+                }
+
 
                 Spacer(modifier = Modifier.height(40.dp))
                 Row {
